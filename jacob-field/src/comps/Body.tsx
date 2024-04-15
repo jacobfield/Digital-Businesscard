d6cdd21b2af7ed77f443260f03bba6fad65f93db
Jake Field <jacobprogrammes@gmail.com>
2024-04-15 15:50:28 +0100

    transparent pic

--- jacob-field/src/comps/Body.tsx
+++ jacob-field/src/comps/Body.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { Main } from "./Main.tsx";
 
 export function Body() {
   return (
@@ -6,6 +7,7 @@ export function Body() {
       <h1 className="header-title">
         <span className="jacob">JACOB </span>&nbsp;FIELD
       </h1>
+      <Main></Main>
     </div>
   );
 }
