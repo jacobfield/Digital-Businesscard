9a9ac3b12b8064600996c5a6617eab98b274e88a
Jake Field <jacobprogrammes@gmail.com>
2024-04-15 16:00:22 +0100

    start of Main Div

--- jacob-field/src/comps/Main.tsx
+++ jacob-field/src/comps/Main.tsx
@@ -1,5 +1,13 @@
 import * as React from "react";
 
 export function Main() {
-  return <></>;
+  return (
+    <div className="arch-div">
+      <img
+        className="profile-pic"
+        src="transparent-profile.png"
+        alt="Jacob Field profile "
+      />
+    </div>
+  );
 }
