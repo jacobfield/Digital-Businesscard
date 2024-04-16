be6f99f28d129e5841415b31eec1daa2e2699ee0
Jake Field <jacobprogrammes@gmail.com>
2024-04-16 15:17:10 +0100

    icons added

--- jacob-field/src/comps/Info.tsx
+++ jacob-field/src/comps/Info.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import { FaLinkedin } from "react-icons/fa";
 import { TiSocialInstagram } from "react-icons/ti";
-
+import { FaSquareGithub } from "react-icons/fa6";
+import { MdOutlineEmail } from "react-icons/md";
+import { ImProfile } from "react-icons/im";
 export function Info() {
   return (
     <div className="info-container">
