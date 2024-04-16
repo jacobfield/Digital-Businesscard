fd0965b69e683b9e956a500816aa26918d1b2bda
Jake Field <jacobprogrammes@gmail.com>
2024-04-16 14:55:14 +0100

    placeholder text

--- jacob-field/src/comps/Main.tsx
+++ jacob-field/src/comps/Main.tsx
@@ -2,12 +2,21 @@ import * as React from "react";
 
 export function Main() {
   return (
-    <div className="arch-div">
-      <img
-        className="profile-pic"
-        src="Linkedin.png"
-        alt="Jacob Field profile "
-      />
-    </div>
+    <>
+      <div className="arch-div">
+        <img
+          className="profile-pic"
+          src="Linkedin.png"
+          alt="Jacob Field profile "
+        />
+      </div>
+      <div className="info-container">
+        <p>Phone Number:</p>
+        <p>Email address:</p>
+        <p>Portfolio:</p>
+        <p>Location:</p>
+        <p>Social media links:</p>
+      </div>
+    </>
   );
 }
